59852128664e434ee6bfadfaf0e0e94c3a57533f
mathn16 <mathias626@hotmail.com>
2022-09-03 10:49:44 +0200

    Added models

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../models/Hero';
+import { HEROES } from '../models/mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  selectedHero?: Hero
+  heroes = HEROES
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSelect(hero: Hero) : void{
+    this.selectedHero = hero
+  }
+
+}
